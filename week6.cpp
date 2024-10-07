b67333b547e61f9f111afabd9f801543b8f181cd
jgaikwad306 <joelgaikwad@outlook.com>
2024-10-06 23:05:15 -0600

    Add files via upload

---
+++ week6.cpp
@@ -0,0 +1,8 @@
+
+echo "# IntroToGithub" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jgaikwad306/IntroToGithub.git
+git push -u origin main
